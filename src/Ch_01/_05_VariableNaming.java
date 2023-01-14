324705e532870b08ed4f0ba276951dcb23ce215d
devery59 <kunhee9045@gmail.com>
2023-01-14 12:02:48 +0900

    second study

---
+++ src/Ch_01/_05_VariableNaming.java
@@ -0,0 +1,14 @@
+package Ch_01;
+
+public class _05_VariableNaming {
+    public static void main(String[] args) {
+        /*1. 저장할 값에 어울리는 이름
+        * 2. 밑줄 문자 숫자 사용 가능(공백 X)
+        * 3.  밑줄 또는 문자로 시작 간으
+        * 4. 한 단어 또는 2개 이상 단어의 연속
+        * 5. 소문자로 시작, 각 단어의 시작 글자는 대문자
+        * 6. 예약어 사용 불가(public, static, void, int, double ...)
+        * */
+
+    }
+}
---
+++ src/Ch_01/_06_Constant.java
@@ -0,0 +1,10 @@
+package Ch_01;
+
+public class _06_Constant {
+    public static void main(String[] args) {
+        final String KR_COUNTRY_CODE = "+82";
+//        KR_COUNTRY_CODE = "+8282";
+        System.out.println(KR_COUNTRY_CODE);
+
+    }
+}
